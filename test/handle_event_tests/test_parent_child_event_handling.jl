d9450a8a915d2b1c372949cfdeda142a7d9c6a90
Andrew Lin <andrew.lin@ieee.org>
2022-03-27 07:45:00 -0700

    Reorganize existing tests.

---
+++ test/handle_event_tests/test_parent_child_event_handling.jl
@@ -0,0 +1,7 @@
+@testset "handle_event() -- Parent state handler not called when child handles event" begin
+
+end
+
+@testset "handle_event() -- Parent state handler called when child does not handle event" begin
+    
+end
